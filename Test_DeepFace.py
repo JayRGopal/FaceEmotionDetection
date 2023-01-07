e9346a3ea3accdc5292ef002ce8e7fddd68c9135
JayRGopal <jay2003@me.com>
2023-01-06 19:55:01 -0500

    updated hyperparams

--- Test_DeepFace.py
+++ Test_DeepFace.py
@@ -1,4 +1,4 @@
 from deepface import DeepFace
 
-DeepFace.stream('/Users/jaygopal/Documents/GitHub/FaceEmotionDetection/image_database')
+DeepFace.stream('/Users/jaygopal/Documents/GitHub/FaceEmotionDetection/image_database', model_name='Facenet', detector_backend='mtcnn', time_threshold=3, frame_threshold=2)
 
