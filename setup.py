fe6772e93ee4fd869891c84b3208eddaa7694550
JayRGopal <jay2003@me.com>
2023-04-17 00:37:41 -0400

    new downloading

--- setup.py
+++ setup.py
@@ -42,39 +42,20 @@ def download_file(link: str, output_dir: str) -> None:
         print("An error occurred while downloading the file.")
     return
 
-def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
+import gdown
 
-    session = requests.Session()
+# Replace 'url' with the URL of your Google Drive file, and 'destination' with the path and filename where you want to save the file.
+def gdownload(fid, dest):
+    url = f'https://drive.google.com/uc?id={fid}'
+    destination = dest
+    gdown.download(url, destination, quiet=False)
+    return
 
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)    
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
 
 #down_now("megraphau/checkpoints/OpenGprahAU-ResNet50_first_stage.pth", '1wnJzvZ8bTR1yc4BhAiNaqU3HH10YX_cf')
 #down_now("megraphau/checkpoints/MEFARG_resnet50_BP4D_fold3.pth", '178lhLCfPKOKlBLj2QgbqQDFEfoXMHEoD')
-download_file_from_google_drive('1wnJzvZ8bTR1yc4BhAiNaqU3HH10YX_cf', 'megraphau/checkpoints/OpenGprahAU-ResNet50_first_stage.pth')
-download_file_from_google_drive('178lhLCfPKOKlBLj2QgbqQDFEfoXMHEoD', 'megraphau/checkpoints/MEFARG_resnet50_BP4D_fold3.pth')
+gdownload('1wnJzvZ8bTR1yc4BhAiNaqU3HH10YX_cf', 'megraphau/checkpoints/OpenGprahAU-ResNet50_first_stage.pth')
+gdownload('178lhLCfPKOKlBLj2QgbqQDFEfoXMHEoD', 'megraphau/checkpoints/MEFARG_resnet50_BP4D_fold3.pth')
 download_file("https://download.pytorch.org/models/resnet50-19c8e357.pth", "megraphau/checkpoints/resnet50-19c8e357.pth")
 
 
