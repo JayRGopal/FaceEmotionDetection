f3cbcc1ff47e818178de7edecc89ff462029d145
JayRGopal <jay2003@me.com>
2023-04-30 20:29:13 -0400

    openface test run

--- main_OpenFace.py
+++ main_OpenFace.py
@@ -8,6 +8,7 @@ Full Pipeline - OpenFace
 
 # Set the parameters
 VIDEO_DIRECTORY = os.path.abspath('inputs/')
+OUTPUT_DIRECTORY = os.path.abspath('outputs_OpenFace/')
 
 # Get the list of all videos in the given directory
 all_videos = [vid for vid in os.listdir(VIDEO_DIRECTORY) if vid[0:1] != '.']
@@ -15,7 +16,7 @@ all_videos = [vid for vid in os.listdir(VIDEO_DIRECTORY) if vid[0:1] != '.']
 # Loop through all videos
 for i in all_videos:
   video_path = os.path.join(VIDEO_DIRECTORY, i)
-  subprocess.Popen(f"C:\Users\DannyHuang\Desktop\OpenFace_2.2.0_win_x64\OpenFace_2.2.0_win_x64\FeatureExtraction.exe -f {video_path} -aus", shell=True)
+  subprocess.Popen(f"C:\Users\DannyHuang\Desktop\OpenFace_2.2.0_win_x64\OpenFace_2.2.0_win_x64\FeatureExtraction.exe -f '{video_path}' -out_dir '{OUTPUT_DIRECTORY}' -aus", shell=True)
 
   
 
