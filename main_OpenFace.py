5793e39157672e23867251a1e83076cd815647e7
JayRGopal <jay2003@me.com>
2023-04-30 20:32:13 -0400

    Update main_OpenFace.py

--- main_OpenFace.py
+++ main_OpenFace.py
@@ -16,7 +16,8 @@ all_videos = [vid for vid in os.listdir(VIDEO_DIRECTORY) if vid[0:1] != '.']
 # Loop through all videos
 for i in all_videos:
   video_path = os.path.join(VIDEO_DIRECTORY, i)
-  subprocess.Popen(f"C:\Users\DannyHuang\Desktop\OpenFace_2.2.0_win_x64\OpenFace_2.2.0_win_x64\FeatureExtraction.exe -f '{video_path}' -out_dir '{OUTPUT_DIRECTORY}' -aus", shell=True)
+  cmd = f'C:\Users\DannyHuang\Desktop\OpenFace_2.2.0_win_x64\OpenFace_2.2.0_win_x64\FeatureExtraction.exe -f "{video_path}" -out_dir "{OUTPUT_DIRECTORY}" -aus' 
+  subprocess.run(cmd, shell=True)
 
   
 
