3c19b3c88ce2527c7efac5762d867b700a8d604b
JayRGopal <jay2003@me.com>
2023-05-04 15:55:22 -0400

    openface checks if video is a file, not a folder!

--- main_OpenFace.py
+++ main_OpenFace.py
@@ -16,12 +16,14 @@ all_videos = [vid for vid in os.listdir(VIDEO_DIRECTORY) if vid[0:1] != '.']
 # Loop through all videos
 for i in all_videos:
   save_file = os.path.join(OUTPUT_DIRECTORY, i[:-4] + '.csv') 
+  video_path = os.path.join(VIDEO_DIRECTORY, i)
   if os.path.exists(save_file):
     print(f'Skipping Video {i}: Output File Already Exists!')
-  else:
-    video_path = os.path.join(VIDEO_DIRECTORY, i)
+  elif os.path.isfile(video_path):
     cmd = f'C:\\Users\\DannyHuang\\Desktop\\OpenFace_2.2.0_win_x64\\OpenFace_2.2.0_win_x64\\FeatureExtraction.exe -f "{video_path}" -out_dir "{OUTPUT_DIRECTORY}" -aus' 
     subprocess.run(cmd, shell=True)
+  else:
+    print(f'WARNING: Got path {video_path}, which is not a valid video file!')
 
   
 
