8de02016ebd921043c8470bf16d52e27c6d8b718
JayRGopal <jay2003@me.com>
2023-06-01 15:30:07 -0400

    Removed old tests

--- Test_DeepFace.py
+++
@@ -1,4 +0,0 @@
-from deepface import DeepFace
-
-DeepFace.stream('/Users/jaygopal/Documents/GitHub/FaceEmotionDetection/image_database', model_name='Facenet', detector_backend='mtcnn', time_threshold=3, frame_threshold=2)
-
--- Test_OpenDBM.py
+++
@@ -1,9 +0,0 @@
-from opendbm import FacialActivity
-
-#make sure Docker is active to access the model
-model = FacialActivity()
-path = "sample.mp4"
-model.fit(path)
-landmark = model.get_landmark()
-
-
