ad0fb92e0b7cfe6fc6c7de1d68694731f8872d47
JayRGopal <jay2003@me.com>
2023-10-02 03:27:37 -0400

    Yolov8 initial commit

---
+++ main_Yolov8.py
@@ -0,0 +1,112 @@
+from utils import *
+from utilsHSE import *
+import os
+import time
+import datetime
+import cv2
+import torch
+
+# Device
+use_cuda = torch.cuda.is_available()
+device = 'cuda:0' if use_cuda else 'cpu'
+if use_cuda:
+  torch.cuda.empty_cache()
+
+
+"""
+
+Full Pipeline - Yolov8 with Tracking
+Verification using DeepFace (Model: VGG-Face)
+
+Success column:
+1: Successful!
+0: Failure: Zero poses
+-1: Failure: Pose detected, and verification failed (across all images in loop!)
+
+"""
+
+# Choose which pipelines to run
+Do_Verification = True 
+
+# Set the parameters
+BATCH_SIZE = 2000
+INPUT_SIZE = (224, 224)
+VIDEO_DIRECTORY = os.path.abspath('inputs/')
+FPS_EXTRACTING = 5 # we'll extract this many fps from the video for analysis
+OUTPUT_DIRECTORY = os.path.abspath('outputs_Yolov8') 
+SUBJECT_FACE_IMAGE_FOLDER = os.path.abspath('deepface/')
+
+# Function that gets us the output folder for each input video
+SAVE_PATH_FOLDER = lambda video_name: os.path.join(OUTPUT_DIRECTORY, f'{video_name}')
+
+# List of unprocessed videos
+unprocessed_videos = get_valid_vids(VIDEO_DIRECTORY, SAVE_PATH_FOLDER)
+
+# For timing estimation
+num_vids = len(unprocessed_videos)
+start_time = time.time()
+
+TIMING_VERBOSE = True # yes/no do we print times for sub-processes within videos?
+
+
+# Loop through all videos
+for i in unprocessed_videos:
+  video_path = os.path.join(VIDEO_DIRECTORY, i)
+
+  frame_now = 0 # this is what we save in outputs file and print
+
+  fps = get_fps(path=video_path, extracting_fps=FPS_EXTRACTING) # FPS at which we're extracting
+
+  # Save paths/folders
+  save_folder_now = SAVE_PATH_FOLDER(i)
+  os.mkdir(save_folder_now)
+  save_path_yolov8 = os.path.join(save_folder_now, f'outputs_yolov8.csv')
+
+
+  # Extract video frames
+  capture = cv2.VideoCapture(video_path)
+  ims = []
+  real_frame_numbers = []
+  real_fps = math.ceil(capture.get(cv2.CAP_PROP_FPS)) # real FPS of the video
+  frame_division = real_fps // FPS_EXTRACTING # Helps us only analyze 5 fps (or close to it)
+  running = True
+  frameNr = 0 # Track frame number
+  while running:
+      # Extract frames continuously
+      success, frame = capture.read()
+      if success:
+          if frameNr % frame_division == 0:
+              # We are only saving SOME frames (e.g. extracting 5 fps)
+              frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+              ims.append(frame)
+              real_frame_numbers.append(frameNr)
+          if (frameNr % BATCH_SIZE == 0) and (frameNr > 0) and len(real_frame_numbers) > 0:
+              # Let's do analysis, save results, and reset ims!
+              ims = np.array(ims)
+              print(f"Extracted Ims, Frames {frame_now} to {frameNr} in {i}") 
+
+              
+      else:
+          # We're out of frames!
+          running = False
+
+          # Let's do analysis, save results, and reset ims!
+          ims = np.array(ims)
+          print(f"Extracted Ims, Frames {frame_now} to {frameNr} in {i}")
+
+          
+      frameNr = frameNr + 1
+  capture.release()
+
+  # Time estimation
+  elapsed_time = time.time() - start_time
+  iterations_left = num_vids - unprocessed_videos.index(i) - 1
+  time_per_iteration = elapsed_time / (unprocessed_videos.index(i) + 1)
+  time_left = time_per_iteration * iterations_left
+  time_left_formatted = str(datetime.timedelta(seconds=int(time_left)))
+  
+  # print an update on the progress
+  print("Approximately ", time_left_formatted, " left to complete analyzing all videos")
+
+ 
+
