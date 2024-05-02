873f0a4ec1fff7c8c0857b215aacff0bcee90c54
JayRGopal <jay2003@me.com>
2024-05-02 00:46:20 -0400

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,1 +1,6 @@
-Danny_Labels.loc[:, Danny_Labels.columns.str.contains('Time')] = Danny_Labels.loc[:, Danny_Labels.columns.str.contains('Time')].applymap(lambda x: x.replace(' ', '') if isinstance(x, str) else x)
+import re
+
+def print_invalid_time_rows(df):
+    pattern = re.compile(r'^\d{2}:\d{2}:\d{2}$')
+    invalid_rows = df[~df['Time Start'].astype(str).str.match(pattern)]
+    print(invalid_rows)
