c916f7973217f7bc9ca7375b03e0cd1797b942ad
JayRGopal <jay2003@me.com>
2024-07-05 03:59:51 -0400

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,2 +1,5 @@
 def merge_dicts(dict1, dict2):
-    return {key: pd.concat([dict1[key], dict2[key]], axis=1) for key in dict1}
+    return {
+        key: pd.concat([dict1[key], dict2[key].drop(columns=['Datetime'], errors='ignore')], axis=1) 
+        for key in dict1
+    }
