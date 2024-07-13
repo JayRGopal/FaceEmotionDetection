dab55ae6920bbe365126f6269f4185ebb4531f11
JayRGopal <jay2003@me.com>
2024-07-13 16:53:33 -0400

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,22 +1,13 @@
-import pandas as pd
-from openpyxl import load_workbook
-
-def save_to_excel(self_reports_dict_list, MOOD_TRACKING_SHEET_PATH):
+def save_to_new_excel(self_reports_dict_list, MOOD_TRACKING_SHEET_PATH):
     # Convert list of dictionaries to DataFrame
     df = pd.DataFrame(self_reports_dict_list)
     
-    # Load the existing Excel file
-    book = load_workbook(MOOD_TRACKING_SHEET_PATH)
+    # Get the directory of the provided sheet path
+    directory = os.path.dirname(MOOD_TRACKING_SHEET_PATH)
     
-    # Create a Pandas Excel writer using openpyxl as the engine
-    with pd.ExcelWriter(MOOD_TRACKING_SHEET_PATH, engine='openpyxl') as writer:
-        writer.book = book
-        # Remove the sheet if it already exists
-        if 'Data_Overview' in writer.book.sheetnames:
-            del writer.book['Data_Overview']
-        
-        # Write DataFrame to a new sheet
+    # Define the new file path
+    new_file_path = os.path.join(directory, 'Mood_Tracking_Overview.xlsx')
+    
+    # Save the DataFrame to the new Excel file
+    with pd.ExcelWriter(new_file_path, engine='openpyxl') as writer:
         df.to_excel(writer, sheet_name='Data_Overview', index=False)
-        
-        # Save the workbook
-        writer.save()
