e614c5d69ca29b91c26a6b7792f7455df21af0e8
JayRGopal <jay2003@me.com>
2024-07-22 02:33:19 -0400

    Update main_EventAnalysis.py

--- main_EventAnalysis.py
+++ main_EventAnalysis.py
@@ -87,8 +87,8 @@ for _, row in tqdm(datetime_df.iterrows(), total=len(datetime_df)):
     video_end = row['VideoEnd']
 
     # Load emotion and AU CSVs
-    emotion_csv_path = os.path.join(CSV_DIRECTORY, video_file.replace('.mp4', ''), 'outputs_hse.csv')
-    au_csv_path = os.path.join(CSV_DIRECTORY, video_file.replace('.mp4', ''), 'outputs_ogau.csv')
+    emotion_csv_path = os.path.join(CSV_DIRECTORY, video_file, 'outputs_hse.csv')
+    au_csv_path = os.path.join(CSV_DIRECTORY, video_file, 'outputs_ogau.csv')
 
     if not os.path.exists(emotion_csv_path) or not os.path.exists(au_csv_path):
         continue
