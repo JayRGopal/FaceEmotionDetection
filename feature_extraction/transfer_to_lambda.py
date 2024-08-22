b54c95e3eb4ebd0ceb80382f4bf56ad7d60c0c93
Jay Gopal <Jay_Gopal@Brown.edu>
2024-08-22 02:44:20 -0700

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,22 +1,29 @@
-# Check and load or generate dfs_hsemotion
-if not os.path.exists(RUNTIME_VAR_PATH + f'dfs_hsemotion_{PAT_SHORT_NAME}.pkl'):
-    # Generate dfs_hsemotion if not already saved
-    dfs_hsemotion = get_dict(COMBINED_OUTPUT_DIRECTORY, file_now='outputs_hse.csv')
-    dfs_hsemotion = apply_function_to_dict(dfs_hsemotion, only_successful_frames)
-    save_var(dfs_hsemotion, forced_name=f'dfs_hsemotion_{PAT_SHORT_NAME}')
-else:
-    # Load dfs_hsemotion if it already exists
-    dfs_hsemotion = load_var(f'dfs_hsemotion_{PAT_SHORT_NAME}')
+And again for this (no need to give me those loading and saving functions you're not changing!')
 
-# Check and load or generate dfs_opengraphau
-if not os.path.exists(RUNTIME_VAR_PATH + f'dfs_opengraphau_{PAT_SHORT_NAME}.pkl'):
-    # Generate dfs_opengraphau if not already saved
-    OPENGRAPHAU_THRESHOLD = 0.5
-    dfs_opengraphau = get_dict(COMBINED_OUTPUT_DIRECTORY, file_now='outputs_ogau.csv')
-    dfs_opengraphau = apply_function_to_dict(dfs_opengraphau, create_binary_columns, threshold=OPENGRAPHAU_THRESHOLD)
-    dfs_opengraphau = apply_function_to_dict(dfs_opengraphau, only_successful_frames)
-    dfs_opengraphau = apply_function_to_dict(dfs_opengraphau, remove_columns_ending_with_r)
-    save_var(dfs_opengraphau, forced_name=f'dfs_opengraphau_{PAT_SHORT_NAME}')
-else:
-    # Load dfs_opengraphau if it already exists
-    dfs_opengraphau = load_var(f'dfs_opengraphau_{PAT_SHORT_NAME}')
+openface_radius_dict = time_splitter(openface_radius_dict, [5, 10])
+hsemotion_radius_dict = time_splitter(hsemotion_radius_dict, [5, 10])
+opengraphau_radius_dict = time_splitter(opengraphau_radius_dict, [5, 10])
+openface_extras_radius_dict = time_splitter(openface_extras_radius_dict, [5, 10])
+
+
+# SAVE VARIABLES - EMOTION DETECTION & AFFECT
+
+save_var(openface_radius_dict, forced_name=f'openface_radius_dict_{PAT_SHORT_NAME}')
+
+save_var(hsemotion_radius_dict, forced_name=f'hsemotion_radius_dict_{PAT_SHORT_NAME}')
+
+save_var(opengraphau_radius_dict, forced_name=f'opengraphau_radius_dict_{PAT_SHORT_NAME}')
+
+save_var(openface_extras_radius_dict, forced_name=f'openface_extras_radius_dict_{PAT_SHORT_NAME}')
+
+
+
+# LOAD VARIABLES - EMOTION DETECTION & AFFECT
+
+openface_radius_dict = load_var(f'openface_radius_dict_{PAT_SHORT_NAME}')
+
+hsemotion_radius_dict = load_var(f'hsemotion_radius_dict_{PAT_SHORT_NAME}')
+
+opengraphau_radius_dict = load_var(f'opengraphau_radius_dict_{PAT_SHORT_NAME}')
+
+openface_extras_radius_dict = load_var(f'openface_extras_radius_dict_{PAT_SHORT_NAME}')
