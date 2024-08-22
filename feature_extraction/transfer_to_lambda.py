9683d3206fb7fbc8de3ff4b94eb078263b257c0c
JayRGopal <jay2003@me.com>
2024-08-22 05:33:25 -0400

    feat: supporting depression and anxiety lasso for core results

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -27,95 +27,92 @@ REMOVE_OUTLIERS = False
 
 
 for RESULTS_PREFIX in RESULTS_PREFIX_LIST:
-  do_lasso = False
-  do_ridge = False
+    do_lasso = False
+    do_ridge = False
 
-  if '_L_' in RESULTS_PREFIX:
-    do_lasso = True
+    if '_L_' in RESULTS_PREFIX:
+        do_lasso = True
 
-  if '_R_' in RESULTS_PREFIX:
-    do_ridge = True
+    if '_R_' in RESULTS_PREFIX:
+        do_ridge = True
 
+    if 'OF_' in RESULTS_PREFIX:
+        spreadsheet_path = FEATURE_LABEL_PATH + f'experimental3_openface_0.5_hours.xlsx'
+        vectors_now = openface_vectors_dict
+        method_now = 'OpenFace'
 
-  if 'OF_' in RESULTS_PREFIX:
-    spreadsheet_path = FEATURE_LABEL_PATH+f'experimental3_openface_0.5_hours.xlsx'
-    vectors_now = openface_vectors_dict
-    method_now = 'OpenFace'
+    elif 'OGAU_' in RESULTS_PREFIX:
+        spreadsheet_path = FEATURE_LABEL_PATH + 'opengraphau_0.5_hours.xlsx'
+        vectors_now = opengraphau_vectors_dict
+        method_now = 'OpenGraphAU'
 
-  elif 'OGAU_' in RESULTS_PREFIX:
-    spreadsheet_path = FEATURE_LABEL_PATH+'opengraphau_0.5_hours.xlsx'
-    vectors_now = opengraphau_vectors_dict
-    method_now = 'OpenGraphAU'
+    elif 'OFAUHSE_' in RESULTS_PREFIX:
+        spreadsheet_path = FEATURE_LABEL_PATH + 'ofauhse_0.5_hours.xlsx'
+        vectors_now = ofauhsemotion_vectors_dict
+        method_now = 'OFAU+HSE'
 
-  elif 'OFAUHSE_' in RESULTS_PREFIX:
-    spreadsheet_path = FEATURE_LABEL_PATH+'ofauhse_0.5_hours.xlsx'
-    vectors_now = ofauhsemotion_vectors_dict
-    method_now = 'OFAU+HSE'
+    elif 'OGAUHSE_' in RESULTS_PREFIX:
+        spreadsheet_path = FEATURE_LABEL_PATH + 'ogauhse_0.5_hours.xlsx'
+        vectors_now = ogauhsemotion_vectors_dict
+        method_now = 'OGAU+HSE'
 
-  elif 'OGAUHSE_' in RESULTS_PREFIX:
-    spreadsheet_path = FEATURE_LABEL_PATH+'ogauhse_0.5_hours.xlsx'
-    vectors_now = ogauhsemotion_vectors_dict
-    method_now = 'OGAU+HSE'
+    elif 'HSE_' in RESULTS_PREFIX:
+        spreadsheet_path = FEATURE_LABEL_PATH + 'hsemotion_0.5_hours.xlsx'
+        vectors_now = hsemotion_vectors_dict
+        method_now = 'HSEmotion'
 
-  elif 'HSE_' in RESULTS_PREFIX:
-    spreadsheet_path = FEATURE_LABEL_PATH+'hsemotion_0.5_hours.xlsx'
-    vectors_now = hsemotion_vectors_dict
-    method_now = 'HSEmotion'
+    elif 'ALL_' in RESULTS_PREFIX:
+        spreadsheet_path = FEATURE_LABEL_PATH + 'all_0.5_hours.xlsx'
+        vectors_now = all_vectors_dict
+        method_now = 'ALL(OF+OG+HSE)'
 
-  elif 'ALL_' in RESULTS_PREFIX:
-    spreadsheet_path = FEATURE_LABEL_PATH+'all_0.5_hours.xlsx'
-    vectors_now = all_vectors_dict
-    method_now = 'ALL(OF+OG+HSE)'
+    # Let's put each setting in its own folder!
+    os.makedirs(RESULTS_PATH_BASE + RESULTS_PREFIX, exist_ok=True)
+    results_prefix_unmodified = RESULTS_PREFIX
+    RESULTS_PREFIX = RESULTS_PREFIX + '/' + RESULTS_PREFIX
 
+    # Loop through metrics (Anxiety, Depression, Mood, etc.)
+    for metric in all_metrics:
+        print('METRIC NOW: ', metric)
+        if do_lasso:
+            alpha_now = best_alphas_lasso[results_prefix_unmodified].get(metric, 1.0)  # Use the specific alpha for the metric
+        elif do_ridge:
+            alpha_now = best_alphas_ridge[results_prefix_unmodified].get(metric, 1.0)  # Use the specific alpha for the metric
+        else:
+            # Neither lasso nor ridge, so alpha is irrelevant
+            alpha_now = 1.0
 
-  # Let's put each setting in its own folder!
-  os.makedirs(RESULTS_PATH_BASE + RESULTS_PREFIX, exist_ok=True)
-  results_prefix_unmodified = RESULTS_PREFIX
-  RESULTS_PREFIX = RESULTS_PREFIX + '/' + RESULTS_PREFIX
+        vectors_return, y = extractOneMetric(metric, vectors_now=vectors_now, remove_outliers=REMOVE_OUTLIERS)
+        scores, preds, y, models = linRegOneMetric(vectors_return, y, do_lasso=do_lasso, do_ridge=do_ridge, alpha=alpha_now)
+        scores_r, preds_r, _, models_r = linRegOneMetric(vectors_return, y, randShuffle=True, alpha=alpha_now)
 
+        # make scatterplots
+        randShuffleR, _, _ = plot_scatterplots(preds_r, y, f'{metric} Random Shuffle', RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_scatterRand{FILE_ENDING}')
+        r_list, p_list, scatterFig = plot_scatterplots(preds, y, metric, RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_scatterplots{FILE_ENDING}', randShuffleR=randShuffleR)
 
-  # Loop through metrics (Anxiety, Depression, Mood, etc.)
-  for metric in all_metrics:
-    print('METRIC NOW: ', metric)
-    if do_lasso:
-      alpha_now = best_alphas_lasso[results_prefix_unmodified]
-    elif do_ridge:
-      alpha_now = best_alphas_ridge[results_prefix_unmodified]
-    else:
-      # Neither lasso nor ridge, so alpha is irrelevant
-      alpha_now = 1.0
+        # Determine our best time radius for this metric based on Pearson's R
+        best_time_radius = list(scores.keys())[np.argmax(r_list)]
+        best_mse_list = scores[best_time_radius]
+        best_avg_mse = np.mean(scores[best_time_radius])
+        best_pearson_r = r_list[np.argmax(r_list)]
 
-    vectors_return, y = extractOneMetric(metric, vectors_now=vectors_now, remove_outliers=REMOVE_OUTLIERS)
-    scores, preds, y, models = linRegOneMetric(vectors_return, y, do_lasso=do_lasso, do_ridge=do_ridge, alpha=alpha_now)
-    scores_r, preds_r, _, models_r = linRegOneMetric(vectors_return, y, randShuffle=True, alpha=alpha_now)
+        # bar plot for pearson r
+        rPlotFig = make_r_barplot(r_list, list(scores.keys()), metric, RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_R{FILE_ENDING}', method_now=method_now)
 
-    # make scatterplots
-    randShuffleR, _, _ = plot_scatterplots(preds_r, y, f'{metric} Random Shuffle', RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_scatterRand{FILE_ENDING}')
-    r_list, p_list, scatterFig = plot_scatterplots(preds, y, metric, RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_scatterplots{FILE_ENDING}', randShuffleR=randShuffleR)
+        # make MSE plot
+        MSEPlotFig = make_mse_boxplot(scores, metric, RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_MSE{FILE_ENDING}', method_now=method_now)
 
-    # Determine our best time radius for this metric based on Pearson's R
-    best_time_radius = list(scores.keys())[np.argmax(r_list)]
-    best_mse_list = scores[best_time_radius]
-    best_avg_mse = np.mean(scores[best_time_radius])
-    best_pearson_r = r_list[np.argmax(r_list)]
+        # Feature ablation
+        # feat_ab_scores, feat_ab_prs = featureAblate(vectors_return[best_time_radius], y, do_lasso=do_lasso, do_ridge=do_ridge)
 
-    # bar plot for pearson r
-    rPlotFig = make_r_barplot(r_list, list(scores.keys()), metric, RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_R{FILE_ENDING}', method_now=method_now)
+        # top_indices, featAbMSEFig = plotFeatAbMSEs(feat_ab_scores, best_mse_list, metric, best_time_radius, savepath=RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_featAblate_MSEs{FILE_ENDING}', spreadsheet_path=spreadsheet_path)
+        # plotFeatAbPRs(feat_ab_prs, best_pearson_r, metric, best_time_radius, savepath=RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_featAblate_R{FILE_ENDING}', spreadsheet_path=spreadsheet_path)
 
-    # make MSE plot
-    MSEPlotFig = make_mse_boxplot(scores, metric, RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_MSE{FILE_ENDING}', method_now=method_now)
+        # extract just ONE scatterplot (the best pearson's R) and save it individually
+        plt.rcParams['lines.markersize'] = 9
+        _, _, bestScatterFig = plot_predictions(y, preds[best_time_radius], randShuffleR=randShuffleR[np.argmax(r_list)], ax=None, time_rad=best_time_radius, metric=metric)
+        bestScatterFig.savefig(RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_bestScatter{FILE_ENDING}', bbox_inches='tight')
 
-    # Feature ablation
-    #feat_ab_scores, feat_ab_prs = featureAblate(vectors_return[best_time_radius], y, do_lasso=do_lasso, do_ridge=do_ridge)
-
-    #top_indices, featAbMSEFig = plotFeatAbMSEs(feat_ab_scores, best_mse_list, metric, best_time_radius, savepath=RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_featAblate_MSEs{FILE_ENDING}', spreadsheet_path=spreadsheet_path)
-    #plotFeatAbPRs(feat_ab_prs, best_pearson_r, metric, best_time_radius, savepath=RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_featAblate_R{FILE_ENDING}', spreadsheet_path=spreadsheet_path)
-
-    # extract just ONE scatterplot (the best pearson's R) and save it individually
-    plt.rcParams['lines.markersize'] = 9
-    _, _, bestScatterFig = plot_predictions(y, preds[best_time_radius], randShuffleR=randShuffleR[np.argmax(r_list)], ax=None, time_rad=best_time_radius, metric=metric)
-    bestScatterFig.savefig(RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_linReg_bestScatter{FILE_ENDING}', bbox_inches='tight')
-
-    # Plot top n features vs. self-reported scores
-    #PLOT_NOW = 3
-    #plot_feat_scatterplots(vectors_array=vectors_return[best_time_radius], y=y, feat_ind_list=top_indices[:PLOT_NOW], metric=metric, savepath=RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_topFeats{FILE_ENDING}', spreadsheet_path=spreadsheet_path)
+        # Plot top n features vs. self-reported scores
+        # PLOT_NOW = 3
+        # plot_feat_scatterplots(vectors_array=vectors_return[best_time_radius], y=y, feat_ind_list=top_indices[:PLOT_NOW], metric=metric, savepath=RESULTS_PATH_BASE + f'{RESULTS_PREFIX}{metric}_topFeats{FILE_ENDING}', spreadsheet_path=spreadsheet_path)
