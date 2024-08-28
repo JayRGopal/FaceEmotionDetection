3d402c800665ee91d6f1ba678e07c97d260bd121
Jay Gopal <Jay_Gopal@Brown.edu>
2024-08-28 01:26:10 -0700

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,12 +1,12 @@
-Traceback (most recent call last):
-  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis.py", line 137, in <module>
-    event_data = pd.merge(event_au_df, event_emotion_df.drop(columns=['frame', 'timestamp', 'success']), on='frame')
-  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/core/reshape/merge.py", line 170, in merge
-    op = _MergeOperation(
-  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/core/reshape/merge.py", line 794, in __init__
-    ) = self._get_merge_keys()
-  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/core/reshape/merge.py", line 1297, in _get_merge_keys
-    right_keys.append(right._get_label_or_level_values(rk))
-  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/core/generic.py", line 1911, in _get_label_or_level_values
-    raise KeyError(key)
-KeyError: 'frame'
+Merging data for event in 3332W600.mp4:
+event_au_df columns: Index(['frame', 'timestamp', 'success', 'AU1', 'AU2', 'AU4', 'AU5', 'AU6',
+       'AU7', 'AU9', 'AU10', 'AU11', 'AU12', 'AU13', 'AU14', 'AU15', 'AU16',
+       'AU17', 'AU18', 'AU19', 'AU20', 'AU22', 'AU23', 'AU24', 'AU25', 'AU26',
+       'AU27', 'AU32', 'AU38', 'AU39', 'AUL1', 'AUR1', 'AUL2', 'AUR2', 'AUL4',
+       'AUR4', 'AUL6', 'AUR6', 'AUL10', 'AUR10', 'AUL12', 'AUR12', 'AUL14',
+       'AUR14'],
+      dtype='object')
+event_emotion_df columns: Index(['frame', 'timestamp', 'success', 'Anger', 'Disgust', 'Fear',
+       'Happiness', 'Neutral', 'Sadness', 'Surprise'],
+      dtype='object')
+Skipping event in 3332W600.mp4: KeyError during merging - 'frame'
