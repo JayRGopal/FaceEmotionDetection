8e27dbc15eeee4c49ee1859ad2036cd89356279e
Jay Gopal <Jay_Gopal@Brown.edu>
2024-08-28 01:16:55 -0700

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,35 +1,12 @@
-# Check and load or generate openface_radius_dict
-if not os.path.exists(RUNTIME_VAR_PATH + f'openface_radius_dict_{PAT_SHORT_NAME}.pkl'):
-    # Generate openface_radius_dict if not already saved
-    openface_radius_dict = time_splitter(openface_radius_dict, [5, 10])
-    save_var(openface_radius_dict, forced_name=f'openface_radius_dict_{PAT_SHORT_NAME}')
-else:
-    # Load openface_radius_dict if it already exists
-    openface_radius_dict = load_var(f'openface_radius_dict_{PAT_SHORT_NAME}')
-
-# Check and load or generate hsemotion_radius_dict
-if not os.path.exists(RUNTIME_VAR_PATH + f'hsemotion_radius_dict_{PAT_SHORT_NAME}.pkl'):
-    # Generate hsemotion_radius_dict if not already saved
-    hsemotion_radius_dict = time_splitter(hsemotion_radius_dict, [5, 10])
-    save_var(hsemotion_radius_dict, forced_name=f'hsemotion_radius_dict_{PAT_SHORT_NAME}')
-else:
-    # Load hsemotion_radius_dict if it already exists
-    hsemotion_radius_dict = load_var(f'hsemotion_radius_dict_{PAT_SHORT_NAME}')
-
-# Check and load or generate opengraphau_radius_dict
-if not os.path.exists(RUNTIME_VAR_PATH + f'opengraphau_radius_dict_{PAT_SHORT_NAME}.pkl'):
-    # Generate opengraphau_radius_dict if not already saved
-    opengraphau_radius_dict = time_splitter(opengraphau_radius_dict, [5, 10])
-    save_var(opengraphau_radius_dict, forced_name=f'opengraphau_radius_dict_{PAT_SHORT_NAME}')
-else:
-    # Load opengraphau_radius_dict if it already exists
-    opengraphau_radius_dict = load_var(f'opengraphau_radius_dict_{PAT_SHORT_NAME}')
-
-# Check and load or generate openface_extras_radius_dict
-if not os.path.exists(RUNTIME_VAR_PATH + f'openface_extras_radius_dict_{PAT_SHORT_NAME}.pkl'):
-    # Generate openface_extras_radius_dict if not already saved
-    openface_extras_radius_dict = time_splitter(openface_extras_radius_dict, [5, 10])
-    save_var(openface_extras_radius_dict, forced_name=f'openface_extras_radius_dict_{PAT_SHORT_NAME}')
-else:
-    # Load openface_extras_radius_dict if it already exists
-    openface_extras_radius_dict = load_var(f'openface_extras_radius_dict_{PAT_SHORT_NAME}')
+Traceback (most recent call last):
+  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis.py", line 130, in <module>
+    event_data = pd.merge(event_au_df, event_emotion_df.drop(columns=['frame', 'timestamp', 'success']), on='frame')
+  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/core/reshape/merge.py", line 170, in merge
+    op = _MergeOperation(
+  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/core/reshape/merge.py", line 794, in __init__
+    ) = self._get_merge_keys()
+  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/core/reshape/merge.py", line 1297, in _get_merge_keys
+    right_keys.append(right._get_label_or_level_values(rk))
+  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/core/generic.py", line 1911, in _get_label_or_level_values
+    raise KeyError(key)
+KeyError: 'frame'
