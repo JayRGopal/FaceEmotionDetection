a9c4805eb52825b647ca0c0ec45b2f485960f9ba
JayRGopal <jay2003@me.com>
2024-08-29 03:57:22 -0400

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,1 +1,1 @@
-/home/jgopal/Desktop/OpenFace/build/bin/FeatureExtraction: error while loading shared libraries: libavcodec.so.58: cannot open shared object file: No such file or directory
+sudo apt install libavcodec58 libavformat58 libavutil56
