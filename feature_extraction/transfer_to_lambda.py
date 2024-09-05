91b4b172038967c83d4640483016f16d682eafac
Jay Gopal <Jay_Gopal@Brown.edu>
2024-09-05 00:50:14 -0700

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,7 +1,15 @@
- 40%|██████████████████                           | 2/5 [00:13<00:19,  6.55s/it]
+100%|█████████████████████████████████████████████| 5/5 [00:21<00:00,  4.38s/it]
 Traceback (most recent call last):
-  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 146, in <module>
-    video_events = detect_events(emotion_df, au_df, openface_df)
-  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 55, in detect_events
-    events[-1]['Duration in Seconds'] = round(events[-1]['Duration in Seconds'] + event_length / FACEDX_FPS, 1)
-KeyError: 'Duration in Seconds'
+  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 160, in <module>
+    events_df.to_csv(OUTPUT_CSV, index=False)
+  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/util/_decorators.py", line 333, in wrapper
+    return func(*args, **kwargs)
+  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/core/generic.py", line 3967, in to_csv
+    return DataFrameRenderer(formatter).to_csv(
+  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/io/formats/format.py", line 1014, in to_csv
+    csv_formatter.save()
+  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/io/formats/csvs.py", line 251, in save
+    with get_handle(
+  File "/home/jgopal/miniconda3/envs/cvquant/lib/python3.10/site-packages/pandas/io/common.py", line 873, in get_handle
+    handle = open(
+FileNotFoundError: [Errno 2] No such file or directory: '/home/jgopal/NAS/Analysis/outputs_EventAnalysis/combined_events_S23_199.csv'
