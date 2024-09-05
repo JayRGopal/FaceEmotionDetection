d5b82ec69934d7470e7a5f57f260c4683e044308
Jay Gopal <Jay_Gopal@Brown.edu>
2024-09-05 00:16:22 -0700

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,14 +1,8 @@
+python3 main_EventAnalysis_AllFrames.py 
+ 40%|██████████████████                           | 2/5 [00:13<00:19,  6.53s/it]
 Traceback (most recent call last):
-  File "/home/jgopal/Desktop/FaceEmotionDetection/main_GroupRPlot.py", line 350, in <module>
-    plt.scatter(r_values_prefix_1, np.full_like(r_values_prefix_1, 1), c=[calculate_variance_percentage(load_var(f'predictions_{patient}_{PREFIX_1 if metric != "Pain" else PREFIX_1_PAIN}', RUNTIME_VAR_PATH)[f'{metric}']['y_true'], metric) for patient in patients], cmap='viridis', s=100, label=LABEL_1)
-  File "/home/jgopal/miniconda3/envs/featureExtract/lib/python3.11/site-packages/matplotlib/pyplot.py", line 3684, in scatter
-    __ret = gca().scatter(
-            ^^^^^^^^^^^^^^
-  File "/home/jgopal/miniconda3/envs/featureExtract/lib/python3.11/site-packages/matplotlib/__init__.py", line 1465, in inner
-    return func(ax, *map(sanitize_sequence, args), **kwargs)
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/jgopal/miniconda3/envs/featureExtract/lib/python3.11/site-packages/matplotlib/axes/_axes.py", line 4670, in scatter
-    self._parse_scatter_color_args(
-  File "/home/jgopal/miniconda3/envs/featureExtract/lib/python3.11/site-packages/matplotlib/axes/_axes.py", line 4489, in _parse_scatter_color_args
-    raise invalid_shape_exception(c.size, xsize) from err
-ValueError: 'c' argument has 14 elements, which is inconsistent with 'x' and 'y' with size 11.
+  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 133, in <module>
+    video_events = detect_events(emotion_df, au_df, openface_df)
+  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 75, in detect_events
+    for frame in range(start_frame, end_frame + 1):
+TypeError: 'numpy.float64' object cannot be interpreted as an integer
