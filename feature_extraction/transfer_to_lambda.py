a137c9c0c64dccbba1fda8ee98799ffadb47c8d0
Jay Gopal <Jay_Gopal@Brown.edu>
2024-09-05 00:44:24 -0700

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,7 +1,7 @@
- 40%|██████████████████                           | 2/5 [00:13<00:19,  6.62s/it]
+ 40%|██████████████████                           | 2/5 [00:13<00:19,  6.55s/it]
 Traceback (most recent call last):
-  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 135, in <module>
+  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 146, in <module>
     video_events = detect_events(emotion_df, au_df, openface_df)
-  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 91, in detect_events
-    frame_data.update(frame_au.to_dict(orient='records')[0])
-IndexError: list index out of range
+  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 55, in detect_events
+    events[-1]['Duration in Seconds'] = round(events[-1]['Duration in Seconds'] + event_length / FACEDX_FPS, 1)
+KeyError: 'Duration in Seconds'
