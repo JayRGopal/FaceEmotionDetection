01d116e84ae6f92f50661238578781b773891841
Jay Gopal <Jay_Gopal@Brown.edu>
2024-09-05 00:24:01 -0700

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,8 +1,7 @@
-python3 main_EventAnalysis_AllFrames.py 
- 40%|██████████████████                           | 2/5 [00:13<00:19,  6.53s/it]
+ 40%|██████████████████                           | 2/5 [00:13<00:19,  6.62s/it]
 Traceback (most recent call last):
-  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 133, in <module>
+  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 135, in <module>
     video_events = detect_events(emotion_df, au_df, openface_df)
-  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 75, in detect_events
-    for frame in range(start_frame, end_frame + 1):
-TypeError: 'numpy.float64' object cannot be interpreted as an integer
+  File "/home/jgopal/Desktop/FaceEmotionDetection/main_EventAnalysis_AllFrames.py", line 91, in detect_events
+    frame_data.update(frame_au.to_dict(orient='records')[0])
+IndexError: list index out of range
