eb788ac58592a5de7f0e8ca9240004c670372753
JayRGopal <jay2003@me.com>
2024-09-28 19:39:45 -0400

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,1 +1,1 @@
-mkdir -p ~/NAS/Conda_Backups/klab && conda env list | awk '{print $1}' | grep -v "#" | xargs -I {} conda env export -n {} > ~/NAS/Conda_Backups/klab/{}.yml
+mkdir -p /mnt/NAS/Conda_Backups/klab && conda env list | awk '{print $1}' | grep -v "#" | xargs -I {} conda env export -n {} > /mnt/NAS/Conda_Backups/klab/{}.yml
