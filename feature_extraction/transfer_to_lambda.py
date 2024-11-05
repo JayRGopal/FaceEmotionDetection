03d3cdb494b92f15d9e085fb992ccf49bdf6710c
JayRGopal <jay2003@me.com>
2024-11-05 18:24:48 -0500

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,1 +1,2 @@
-find $CONDA_PREFIX/lib -name "libIlmImf*.so*"
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+echo 'export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
