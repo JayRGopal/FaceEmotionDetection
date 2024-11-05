747cb496535bcb9f297123b037e7319eb573268d
Jay Gopal <Jay_Gopal@Brown.edu>
2024-11-05 14:17:05 -0800

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,8 +1,107 @@
-./configure --enable-gpl --enable-libx264 --enable-libx265 --enable-nonfree
-ERROR: x265 not found using pkg-config
-
-If you think configure made a mistake, make sure you are using the latest
-version from Git.  If the latest version fails, report the problem to the
-ffmpeg-user@ffmpeg.org mailing list or IRC #ffmpeg on irc.freenode.net.
-Include the log file "ffbuild/config.log" produced by configure as this will help
-solve the problem.
+sudo make install
+HOSTCC	doc/print_options.o
+HOSTLD	doc/print_options
+GENTEXI	doc/avoptions_format.texi
+GENTEXI	doc/avoptions_codec.texi
+POD	doc/ffmpeg.pod
+MAN	doc/ffmpeg.1
+POD	doc/ffprobe.pod
+MAN	doc/ffprobe.1
+POD	doc/ffmpeg-all.pod
+MAN	doc/ffmpeg-all.1
+POD	doc/ffprobe-all.pod
+MAN	doc/ffprobe-all.1
+POD	doc/ffmpeg-utils.pod
+MAN	doc/ffmpeg-utils.1
+POD	doc/ffmpeg-scaler.pod
+MAN	doc/ffmpeg-scaler.1
+POD	doc/ffmpeg-resampler.pod
+MAN	doc/ffmpeg-resampler.1
+POD	doc/ffmpeg-codecs.pod
+MAN	doc/ffmpeg-codecs.1
+POD	doc/ffmpeg-bitstream-filters.pod
+MAN	doc/ffmpeg-bitstream-filters.1
+POD	doc/ffmpeg-formats.pod
+MAN	doc/ffmpeg-formats.1
+POD	doc/ffmpeg-protocols.pod
+MAN	doc/ffmpeg-protocols.1
+POD	doc/ffmpeg-devices.pod
+MAN	doc/ffmpeg-devices.1
+POD	doc/ffmpeg-filters.pod
+MAN	doc/ffmpeg-filters.1
+POD	doc/libavutil.pod
+MAN	doc/libavutil.3
+POD	doc/libswscale.pod
+MAN	doc/libswscale.3
+POD	doc/libswresample.pod
+MAN	doc/libswresample.3
+POD	doc/libavcodec.pod
+MAN	doc/libavcodec.3
+POD	doc/libavformat.pod
+MAN	doc/libavformat.3
+POD	doc/libavdevice.pod
+MAN	doc/libavdevice.3
+POD	doc/libavfilter.pod
+MAN	doc/libavfilter.3
+INSTALL	doc/ffmpeg.1
+INSTALL	doc/ffprobe.1
+INSTALL	doc/ffmpeg-all.1
+INSTALL	doc/ffprobe-all.1
+INSTALL	doc/ffmpeg-utils.1
+INSTALL	doc/ffmpeg-scaler.1
+INSTALL	doc/ffmpeg-resampler.1
+INSTALL	doc/ffmpeg-codecs.1
+INSTALL	doc/ffmpeg-bitstream-filters.1
+INSTALL	doc/ffmpeg-formats.1
+INSTALL	doc/ffmpeg-protocols.1
+INSTALL	doc/ffmpeg-devices.1
+INSTALL	doc/ffmpeg-filters.1
+INSTALL	doc/libavutil.3
+INSTALL	doc/libswscale.3
+INSTALL	doc/libswresample.3
+INSTALL	doc/libavcodec.3
+INSTALL	doc/libavformat.3
+INSTALL	doc/libavdevice.3
+INSTALL	doc/libavfilter.3
+INSTALL	doc/ffmpeg.1
+INSTALL	doc/ffprobe.1
+INSTALL	doc/ffmpeg-all.1
+INSTALL	doc/ffprobe-all.1
+INSTALL	doc/ffmpeg-utils.1
+INSTALL	doc/ffmpeg-scaler.1
+INSTALL	doc/ffmpeg-resampler.1
+INSTALL	doc/ffmpeg-codecs.1
+INSTALL	doc/ffmpeg-bitstream-filters.1
+INSTALL	doc/ffmpeg-formats.1
+INSTALL	doc/ffmpeg-protocols.1
+INSTALL	doc/ffmpeg-devices.1
+INSTALL	doc/ffmpeg-filters.1
+INSTALL	doc/libavutil.3
+INSTALL	doc/libswscale.3
+INSTALL	doc/libswresample.3
+INSTALL	doc/libavcodec.3
+INSTALL	doc/libavformat.3
+INSTALL	doc/libavdevice.3
+INSTALL	doc/libavfilter.3
+CC	libavformat/adtsenc.o
+./libavcodec/x86/mathops.h: Assembler messages:
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+./libavcodec/x86/mathops.h:125: Error: operand type mismatch for `shr'
+make: *** [ffbuild/common.mak:59: libavformat/adtsenc.o] Error 1
