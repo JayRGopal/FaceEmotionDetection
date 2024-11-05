492cec931bc27c235ae9f2ab8e591eb9cd8ffef5
JayRGopal <jay2003@me.com>
2024-11-05 17:29:05 -0500

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,13 +1,4 @@
-find . -name "libavcodec.so*"
-./miniconda3/pkgs/ffmpeg-4.3-hf484d3e_0/lib/libavcodec.so
-./miniconda3/pkgs/ffmpeg-4.3-hf484d3e_0/lib/libavcodec.so.58.91.100
-./miniconda3/pkgs/ffmpeg-4.3-hf484d3e_0/lib/libavcodec.so.58
-./miniconda3/pkgs/ffmpeg-4.2.2-h20bf706_0/lib/libavcodec.so
-./miniconda3/pkgs/ffmpeg-4.2.2-h20bf706_0/lib/libavcodec.so.58.54.100
-./miniconda3/pkgs/ffmpeg-4.2.2-h20bf706_0/lib/libavcodec.so.58
-./miniconda3/envs/cvmonitor/lib/libavcodec.so
-./miniconda3/envs/cvmonitor/lib/libavcodec.so.58.54.100
-./miniconda3/envs/cvmonitor/lib/libavcodec.so.58
-./miniconda3/envs/cvquant/lib/libavcodec.so
-./miniconda3/envs/cvquant/lib/libavcodec.so.58.91.100
-./miniconda3/envs/cvquant/lib/libavcodec.so.58
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+echo 'export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+
