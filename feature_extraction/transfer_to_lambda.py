bac70022a7a24fa39cca79b13c8c683e8368b6ca
Jay Gopal <Jay_Gopal@Brown.edu>
2024-11-05 14:13:30 -0800

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,8 +1,16 @@
 ./configure --enable-gpl --enable-libx264 --enable-libx265 --enable-nonfree
-nasm/yasm not found or too old. Use --disable-x86asm for a crippled build.
+ERROR: libx264 not found
 
 If you think configure made a mistake, make sure you are using the latest
 version from Git.  If the latest version fails, report the problem to the
 ffmpeg-user@ffmpeg.org mailing list or IRC #ffmpeg on irc.freenode.net.
 Include the log file "ffbuild/config.log" produced by configure as this will help
 solve the problem.
+(cvquant) jgopal@klab-lambda-vector:~/ffmpeg-4.3.1$ ls
+Changelog         COPYING.LGPLv3  libavdevice    libswscale   RELEASE_NOTES
+compat            CREDITS         libavfilter    LICENSE.md   tests
+configure         doc             libavformat    MAINTAINERS  tools
+CONTRIBUTING.md   ffbuild         libavresample  Makefile     VERSION
+COPYING.GPLv2     fftools         libavutil      presets
+COPYING.GPLv3     INSTALL.md      libpostproc    README.md
+COPYING.LGPLv2.1  libavcodec      libswresample  RELEASE
