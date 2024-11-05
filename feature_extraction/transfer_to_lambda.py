ae57e19692fb78bdd1a992d7a4991ed9047526a2
Jay Gopal <Jay_Gopal@Brown.edu>
2024-11-05 15:06:40 -0800

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,4 +1,2 @@
-export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-
-echo 'export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+/home/jgopal/Desktop/OpenFace/build/bin/FeatureExtraction: error while loading shared libraries: libIlmImf-2_5.so.25: cannot open shared object file: No such file or directory
 
