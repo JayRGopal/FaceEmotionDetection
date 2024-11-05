2034b7cb2c5fe3cb648d7b99b29536a31b62054a
Jay Gopal <Jay_Gopal@Brown.edu>
2024-11-05 14:28:25 -0800

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,1 +1,13 @@
-find /usr/local/lib /usr/lib -name "libavcodec.so.58"
+find . -name "libavcodec.so*"
+./miniconda3/pkgs/ffmpeg-4.3-hf484d3e_0/lib/libavcodec.so
+./miniconda3/pkgs/ffmpeg-4.3-hf484d3e_0/lib/libavcodec.so.58.91.100
+./miniconda3/pkgs/ffmpeg-4.3-hf484d3e_0/lib/libavcodec.so.58
+./miniconda3/pkgs/ffmpeg-4.2.2-h20bf706_0/lib/libavcodec.so
+./miniconda3/pkgs/ffmpeg-4.2.2-h20bf706_0/lib/libavcodec.so.58.54.100
+./miniconda3/pkgs/ffmpeg-4.2.2-h20bf706_0/lib/libavcodec.so.58
+./miniconda3/envs/cvmonitor/lib/libavcodec.so
+./miniconda3/envs/cvmonitor/lib/libavcodec.so.58.54.100
+./miniconda3/envs/cvmonitor/lib/libavcodec.so.58
+./miniconda3/envs/cvquant/lib/libavcodec.so
+./miniconda3/envs/cvquant/lib/libavcodec.so.58.91.100
+./miniconda3/envs/cvquant/lib/libavcodec.so.58
