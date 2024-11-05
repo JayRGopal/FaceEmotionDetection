cac7dbc32c7581c291906da39636b187ba24aa4c
Jay Gopal <Jay_Gopal@Brown.edu>
2024-11-05 14:14:28 -0800

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,16 +1,8 @@
 ./configure --enable-gpl --enable-libx264 --enable-libx265 --enable-nonfree
-ERROR: libx264 not found
+ERROR: x265 not found using pkg-config
 
 If you think configure made a mistake, make sure you are using the latest
 version from Git.  If the latest version fails, report the problem to the
 ffmpeg-user@ffmpeg.org mailing list or IRC #ffmpeg on irc.freenode.net.
 Include the log file "ffbuild/config.log" produced by configure as this will help
 solve the problem.
-(cvquant) jgopal@klab-lambda-vector:~/ffmpeg-4.3.1$ ls
-Changelog         COPYING.LGPLv3  libavdevice    libswscale   RELEASE_NOTES
-compat            CREDITS         libavfilter    LICENSE.md   tests
-configure         doc             libavformat    MAINTAINERS  tools
-CONTRIBUTING.md   ffbuild         libavresample  Makefile     VERSION
-COPYING.GPLv2     fftools         libavutil      presets
-COPYING.GPLv3     INSTALL.md      libpostproc    README.md
-COPYING.LGPLv2.1  libavcodec      libswresample  RELEASE
