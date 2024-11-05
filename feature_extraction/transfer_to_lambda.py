ccc143f5c8d2a110861421d634f94d046b351208
Jay Gopal <Jay_Gopal@Brown.edu>
2024-11-05 14:07:22 -0800

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,3 +1,14 @@
-/home/jgopal/Desktop/OpenFace/build/bin/FeatureExtraction: error while loading shared libraries: libavcodec.so.58: cannot open shared object file: No such file or directory
+sudo apt install -y libavcodec58 libavformat58 libavutil58
+Reading package lists... Done
+Building dependency tree... Done
+Reading state information... Done
+Package libavcodec58 is not available, but is referred to by another package.
+This may mean that the package is missing, has been obsoleted, or
+is only available from another source
 
+Package libavformat58 is not available, but is referred to by another package.
+This may mean that the package is missing, has been obsoleted, or
+is only available from another source
 
+E: Package 'libavcodec58' has no installation candidate
+E: Package 'libavformat58' has no installation candidate
