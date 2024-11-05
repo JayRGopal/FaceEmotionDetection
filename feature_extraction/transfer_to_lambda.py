4791790349b361d362742edfc0dad705bd08c8e3
JayRGopal <jay2003@me.com>
2024-11-05 17:26:10 -0500

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,10 +1,1 @@
-wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-tar xzf yasm-1.3.0.tar.gz
-cd yasm-1.3.0
-./configure
-make
-sudo make install
-
-
-./configure --enable-gpl --enable-libx264 --enable-libx265 --enable-nonfree --disable-asm
-make
+find /usr/local/lib /usr/lib -name "libavcodec.so.58"
