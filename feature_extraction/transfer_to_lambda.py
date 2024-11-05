78033eebf9ee761920e8ee16ca3ddb637bca04d8
Jay Gopal <Jay_Gopal@Brown.edu>
2024-11-05 14:09:02 -0800

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,14 +1,9 @@
-sudo apt install -y libavcodec58 libavformat58 libavutil58
+sudo apt install libavcodec-extra libavformat58 libavutil58
 Reading package lists... Done
 Building dependency tree... Done
 Reading state information... Done
-Package libavcodec58 is not available, but is referred to by another package.
-This may mean that the package is missing, has been obsoleted, or
-is only available from another source
-
 Package libavformat58 is not available, but is referred to by another package.
 This may mean that the package is missing, has been obsoleted, or
 is only available from another source
 
-E: Package 'libavcodec58' has no installation candidate
 E: Package 'libavformat58' has no installation candidate
