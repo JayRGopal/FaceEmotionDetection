0e35460aa7d5984b0fb39e8c5ca7a410dc583fe3
JayRGopal <jay2003@me.com>
2024-11-05 23:14:53 -0500

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,5 +1,2 @@
-echo $LD_LIBRARY_PATH
-/home/jgopal/miniconda3/envs/cvquant/lib
-
-/home/jgopal/Desktop/OpenFace/build/bin/FeatureExtraction: error while loading shared libraries: libIlmImf-2_5.so.25: cannot open shared object file: No such file or directory
+find $CONDA_PREFIX/lib -name "libIlmImf-2_5.so.25"
 
