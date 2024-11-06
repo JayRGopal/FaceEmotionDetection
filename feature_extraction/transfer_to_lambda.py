eff949e05ed4f7babfe46b01223343f5a45e1408
Jay Gopal <Jay_Gopal@Brown.edu>
2024-11-05 20:18:35 -0800

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,2 +1,9 @@
 find $CONDA_PREFIX/lib -name "libIlmImf-2_5.so.25"
 
+find . -name "libIlmImf*"
+./libIlmImf.so.25
+./libIlmImfUtil.so
+./libIlmImf.so
+./libIlmImfUtil.so.25
+./libIlmImfUtil.so.25.0.4
+./libIlmImf.so.25.0.4
