dd99023a9dc71ed1ee8f454bfc29b88a29add1c0
JayRGopal <jay2003@me.com>
2024-11-05 23:20:08 -0500

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,9 +1,5 @@
-find $CONDA_PREFIX/lib -name "libIlmImf-2_5.so.25"
+# Step 1: Link libIlmImf.so.25 from the conda environment to /usr/local/lib
+sudo ln -s $CONDA_PREFIX/lib/libIlmImf.so.25 /usr/local/lib/libIlmImf-2_5.so.25
 
-find . -name "libIlmImf*"
-./libIlmImf.so.25
-./libIlmImfUtil.so
-./libIlmImf.so
-./libIlmImfUtil.so.25
-./libIlmImfUtil.so.25.0.4
-./libIlmImf.so.25.0.4
+# Step 2: Update the system's library cache
+sudo ldconfig
