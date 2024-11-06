c2931bee6a54ac193250ab70bcc05492656c3562
JayRGopal <jay2003@me.com>
2024-11-05 23:11:54 -0500

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,2 +1,8 @@
+# Step 1: Clear LD_LIBRARY_PATH to start fresh
+unset LD_LIBRARY_PATH
+
+# Step 2: Set it only once with the current Conda environment's lib directory
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib
+
+# Step 3: Confirm the path now contains only one entry
 echo $LD_LIBRARY_PATH
-/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvquant/lib:/home/jgopal/miniconda3/envs/cvmonitor/lib:/home/jgopal/miniconda3/envs/cvquant/lib:
