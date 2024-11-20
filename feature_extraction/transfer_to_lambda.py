9d1ac48dd13a9b75f0aca73f9adde062910ff7a8
Jay Gopal <Jay_Gopal@Brown.edu>
2024-11-20 09:14:28 -0800

    Update transfer_to_lambda.py

--- feature_extraction/transfer_to_lambda.py
+++ feature_extraction/transfer_to_lambda.py
@@ -1,5 +1,20 @@
-# Step 1: Link libIlmImf.so.25 from the conda environment to /usr/local/lib
-sudo ln -s $CONDA_PREFIX/lib/libIlmImf.so.25 /usr/local/lib/libIlmImf-2_5.so.25
+Total Patients Processed: 30
+Patients with Data: 26
 
-# Step 2: Update the system's library cache
-sudo ldconfig
+Mood Statistics:
+Mean Num Distinct Scores: 3.70
+Median Num Distinct Scores: 4.00
+Mean Num Total Scores: 14.70
+Median Num Total Scores: 13.00
+
+Depression Statistics:
+Mean Num Distinct Scores: 2.80
+Median Num Distinct Scores: 2.00
+Mean Num Total Scores: 14.73
+Median Num Total Scores: 13.00
+
+Anxiety Statistics:
+Mean Num Distinct Scores: 3.53
+Median Num Distinct Scores: 3.00
+Mean Num Total Scores: 14.67
+Median Num Total Scores: 13.00
