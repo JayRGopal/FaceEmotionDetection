b6bd474a9aa044a595d76159c0cd92a9893829e9
JayRGopal <jay2003@me.com>
2025-03-30 02:11:29 -0400

    Update main_Bootstrap.py

--- main_Bootstrap.py
+++ main_Bootstrap.py
@@ -24,7 +24,7 @@ N_BOOTSTRAPS = 2
 ALPHAS = np.linspace(0.1, 2.0, 20)
 TIME_WINDOWS = list(range(15, 241, 15))
 INTERNAL_STATES = ['Mood']
-RESULTS_PREFIX_LIST = ['OF_L_', 'OGAU_L_', 'OGAUHSE_L_', 'HSE_L_']
+RESULTS_PREFIX_LIST = ['OGAUHSE_L_', 'HSE_L_']
 
 os.makedirs(RESULTS_OUTPUT_PATH, exist_ok=True)
 
