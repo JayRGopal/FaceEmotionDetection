580cdbbab2118e9d99fa6eb9ef332d929623769d
JayRGopal <jay2003@me.com>
2025-05-08 01:49:22 -0400

    fix: pyfeat face visualizer

--- main_PyFeat_Visualization.py
+++ main_PyFeat_Visualization.py
@@ -12,8 +12,9 @@ METRIC_NOW = 'Mood'
 os.makedirs(RESULTS_PATH_BASE, exist_ok=True)
 
 # Define AU activations manually (e.g., 1 = activated, 0 = not activated, or intensities 0-3)
-# Ordered AUs are: 1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 14, 15, 17, 20, 23, 24, 25, 26, 28, 43
-au_order = [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 14, 15, 17, 20, 23, 24, 25, 26, 28, 43]
+# Ordered AUs are: 1, 2, 4, 5, 6, 7, 9, 11, 12, 14, 15, 17, 20, 23, 24, 25, 26, 27, 28, 43
+# TODO: Need to figure out the actual missing AU within PyFeat documentation! Filled in 27 for now.
+au_order = [1, 2, 4, 5, 6, 7, 9, 11, 12, 14, 15, 17, 20, 23, 24, 25, 26, 27, 28, 43]
 au_activations = {
     1: 3,  # AU1 activated at intensity 3
     2: 0,  # AU2 not activated
